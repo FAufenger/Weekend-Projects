0225e4f99544ecb185a9ac0383addb2ab5a799e8
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-01-20 11:25:44 -0500

    alien invasion
    now has play button and resets after you lose your 3 lives

--- python_projects/alien_invaders/alien_invasion_game.py
+++ python_projects/alien_invaders/alien_invasion_game.py
@@ -17,6 +17,7 @@ from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 class AlienInvasion:
     """Create an overall class to manage game assets and actions."""
@@ -47,6 +48,9 @@ class AlienInvasion:
 
         self._create_fleet()
 
+        # Make the Play button
+        self.play_button = Button(self, "Play")
+
         
     def run_game(self):
         """Start main loop for the game."""
@@ -68,6 +72,26 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the payer clicks Play."""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        # To disactivate invisable play button when game active
+        if button_clicked and not self.stats.game_active:
+            # Reset the statistics
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+            # Hide the mouse while in play
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypress."""
@@ -161,6 +185,7 @@ class AlienInvasion:
             sleep(1.0)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -218,6 +243,9 @@ class AlienInvasion:
             bullet.draw_bullet()
         # Alien parameters in alien.py
         self.aliens.draw(self.screen)
+        # Draw the play button if the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         # Make the most recently drawn screen visible and hide old
         pygame.display.flip()
 
